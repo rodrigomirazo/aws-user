1433f3cdccd9f8b5127efd5006452f4266e47fd8
miro8001 <rodrigo.mirazo.consultant@nielsen.com>
2020-01-06 11:25:05 -0600

    created empty psring boot project

---
+++ src/test/java/com/aws/dev/cert/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.dev.cert.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
